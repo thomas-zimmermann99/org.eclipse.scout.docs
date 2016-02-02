1e87fa55f607d0de29c221fd7d29cb6a08b958f2
Jeremie Bresson <jeremie.bresson@bsi-software.com>
2016-02-02 16:46:02 +0100

    Rename "widgets" app.
    "org.eclipsescout.demo.widgets" -> "org.eclipse.scout.widgets",
    "org.eclipsescout.demo.widgets.client" -> "org.eclipse.scout.widgets.client",
    "org.eclipsescout.demo.widgets.client.mobile" -> "org.eclipse.scout.widgets.client.mobile",
    "org.eclipsescout.demo.widgets.client.old" -> "org.eclipse.scout.widgets.old.client",
    "org.eclipsescout.demo.widgets.shared" -> "org.eclipse.scout.widgets.shared",
    "org.eclipsescout.demo.widgets.shared.old" -> "org.eclipse.scout.widgets.old.shared",
    "org.eclipsescout.demo.widgets.ui.html" -> "org.eclipse.scout.widgets.ui.html",
    "org.eclipsescout.demo.widgets.ui.html.app.dev" -> "org.eclipse.scout.widgets.ui.html.app.dev",
    "org.eclipsescout.demo.widgets.ui.html.app.war" -> "org.eclipse.scout.widgets.ui.html.app.war"

---
+++ code/widgets/org.eclipse.scout.widgets.client/src/main/java/org/eclipse/scout/widgets/client/ui/desktop/outlines/AdvancedWidgetsOutline.java
@@ -0,0 +1,65 @@
+/*******************************************************************************
+ * Copyright (c) 2015 BSI Business Systems Integration AG.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Distribution License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/org/documents/edl-v10.html
+ *
+ * Contributors:
+ *     BSI Business Systems Integration AG - initial API and implementation
+ ******************************************************************************/
+package org.eclipse.scout.widgets.client.ui.desktop.outlines;
+
+import java.util.List;
+
+import org.eclipse.scout.rt.client.ui.desktop.outline.AbstractOutline;
+import org.eclipse.scout.rt.client.ui.desktop.outline.pages.IPage;
+import org.eclipse.scout.rt.shared.TEXTS;
+import org.eclipse.scout.widgets.client.ui.desktop.pages.FormPage;
+import org.eclipse.scout.widgets.client.ui.forms.HtmlFieldForm;
+import org.eclipse.scout.widgets.client.ui.forms.ImageFieldForm;
+import org.eclipse.scout.widgets.client.ui.forms.ListBoxForm;
+import org.eclipse.scout.widgets.client.ui.forms.SmartFieldForm;
+import org.eclipse.scout.widgets.client.ui.forms.SvgFieldForm;
+import org.eclipse.scout.widgets.client.ui.forms.TableFieldForm;
+import org.eclipse.scout.widgets.client.ui.forms.TreeBoxForm;
+import org.eclipse.scout.widgets.client.ui.forms.TreeFieldForm;
+
+/**
+ * @author mzi
+ */
+public class AdvancedWidgetsOutline extends AbstractOutline {
+
+  @Override
+  protected String getConfiguredTitle() {
+    return TEXTS.get("AdvancedWidgets");
+  }
+
+  @Override
+  protected void execCreateChildPages(List<IPage<?>> pageList) {
+
+    FormPage listBoxFieldPage = new FormPage(ListBoxForm.class);
+    pageList.add(listBoxFieldPage);
+
+    FormPage treeBoxPage = new FormPage(TreeBoxForm.class);
+    pageList.add(treeBoxPage);
+
+    FormPage smartFieldPage = new FormPage(SmartFieldForm.class);
+    pageList.add(smartFieldPage);
+
+    FormPage treeFieldPage = new FormPage(TreeFieldForm.class);
+    pageList.add(treeFieldPage);
+
+    FormPage tableFieldPage = new FormPage(TableFieldForm.class);
+    pageList.add(tableFieldPage);
+
+    FormPage htmlFieldPage = new FormPage(HtmlFieldForm.class);
+    pageList.add(htmlFieldPage);
+
+    FormPage imageFieldPage = new FormPage(ImageFieldForm.class);
+    pageList.add(imageFieldPage);
+
+    FormPage svgFieldPage = new FormPage(SvgFieldForm.class);
+    pageList.add(svgFieldPage);
+  }
+}
