9d97cc6c87f1a0b745eaa7e2b43f379d46829c78
Judtih Gull <jgu@bsiag.com>
2016-05-02 18:07:32 +0200

    convert heatmapfield to class

--- code/widgets/org.eclipse.scout.widgets.heatmap.ui.html/src/main/ts/heatmap/HeatmapField.ts
+++ code/widgets/org.eclipse.scout.widgets.heatmap.ui.html/src/main/ts/heatmap/HeatmapField.ts
@@ -3,110 +3,124 @@ declare var L: any;
 module scout {
     //    "use strict";
 
-
-    declare class FormField { };
-
-    export var HeatmapField = function() {
-        (< any > HeatmapField).parent.call(this);
-    };
-    (<any>scout).inherits(HeatmapField, (<any>scout).FormField);
-
-    /** This is a description of the foo function. */
-    HeatmapField.prototype._render = function($parent) {
-        this.addContainer($parent, 'heatmap-field');
-        this.addLabel();
-        this.addStatus();
-
-        var heatmapId = (<any>scout).objectFactory.createUniqueId();
-        var $field = this.$container
-            .makeDiv('heatmap')
-            .attr('id', heatmapId);
-        this.addField($field);
-
-        // Before (!) installing the layout, set the initial size to 1x1. The size of $field must not
-        // get smaller than that, because Leaflet.js throws an error when the drawing canvas has size 0.
-        // After the initial rendering, this condition is ensured by HeapmapFieldLayout.js.
-        (<any>scout).graphics.setSize($field, new (<any>scout).Dimension(1, 1));
-        var fieldHtmlComp = new (<any>scout).HtmlComponent($field, this.session);
-        fieldHtmlComp.setLayout(new HeatmapFieldLayout(this));
-
-        this.heatmap = L.map(heatmapId, {
-            trackResize: false
-        });
-        L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
-            attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors',
-        }).addTo(this.heatmap);
-
-        this.heatmap.on('zoomend', this._handleViewParameterChanged.bind(this));
-        this.heatmap.on('moveend', this._handleViewParameterChanged.bind(this));
-        this.heatmap.on('click', this._handleClicked.bind(this));
-        this.heatmap.on('contextmenu', this._handleClicked.bind(this));
+    export declare class FormField {
+        _renderProperties(): void;
+        _remove(): any;
+        _send: any;
+        onModelAction(any): any;
+        addContainer(any, string): any;
+        addLabel(): void;
+        addStatus(): void;
+        addField(any): void;
+        invalidateSize(): void;
     };
 
-    HeatmapField.prototype._renderProperties = function() {
-        (< any > HeatmapField).parent.prototype._renderProperties.call(this);
-        this._renderViewParameter();
-        this._renderHeatPointList();
-    };
+    export class HeatmapField extends scout.FormField {
+        public heatmap: any;
+        private _heatLayer: any;
+        private viewParameter: any;
+        private heatPointList: any;
+        private $container: any;
+        private session: any;
 
-    HeatmapField.prototype._remove = function() {
-        (< any > HeatmapField).parent.prototype._remove.call(this);
+        /** This is a description of the foo function. */
+        _render($parent) {
+            this.addContainer($parent, 'heatmap-field');
+            this.addLabel();
+            this.addStatus();
 
-        this.heatmap.remove();
-        this.heatmap = null;
-        this._heatLayer = null;
-    };
+            var heatmapId = (<any>scout).objectFactory.createUniqueId();
+            var $field = this.$container
+                .makeDiv('heatmap')
+                .attr('id', heatmapId);
+            this.addField($field);
 
-    HeatmapField.prototype._handleViewParameterChanged = function() {
-        this._send('viewParameterChanged', {
-            center: {
-                x: this.heatmap.getCenter().lat,
-                y: this.heatmap.getCenter().lng
-            },
-            zoomFactor: this.heatmap.getZoom()
-        });
-    };
+            // Before (!) installing the layout, set the initial size to 1x1. The size of $field must not
+            // get smaller than that, because Leaflet.js throws an error when the drawing canvas has size 0.
+            // After the initial rendering, this condition is ensured by HeapmapFieldLayout.js.
+            (<any>scout).graphics.setSize($field, new (<any>scout).Dimension(1, 1));
+            var fieldHtmlComp = new (<any>scout).HtmlComponent($field, this.session);
+            fieldHtmlComp.setLayout(new HeatmapFieldLayout(this));
 
-    HeatmapField.prototype._handleClicked = function(event) {
-        this._send('clicked', {
-            point: {
-                x: event.latlng.lat,
-                y: event.latlng.lng
+            this.heatmap = L.map(heatmapId, {
+                trackResize: false
+            });
+            L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+                attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors',
+            }).addTo(this.heatmap);
+
+            this.heatmap.on('zoomend', this._handleViewParameterChanged.bind(this));
+            this.heatmap.on('moveend', this._handleViewParameterChanged.bind(this));
+            this.heatmap.on('click', this._handleClicked.bind(this));
+            this.heatmap.on('contextmenu', this._handleClicked.bind(this));
+        };
+
+        _renderProperties() {
+            super._renderProperties();
+            this._renderViewParameter();
+            this._renderHeatPointList();
+        };
+
+        _remove() {
+            super._remove();
+
+            this.heatmap.remove();
+            this.heatmap = null;
+            this._heatLayer = null;
+        };
+
+        _handleViewParameterChanged() {
+            this._send('viewParameterChanged', {
+                center: {
+                    x: this.heatmap.getCenter().lat,
+                    y: this.heatmap.getCenter().lng
+                },
+                zoomFactor: this.heatmap.getZoom()
+            });
+        };
+
+        _handleClicked(event) {
+            this._send('clicked', {
+                point: {
+                    x: event.latlng.lat,
+                    y: event.latlng.lng
+                }
+            });
+        };
+
+        _renderViewParameter() {
+            this.heatmap.setView([this.viewParameter.center.x, this.viewParameter.center.y], this.viewParameter.zoomFactor);
+        };
+
+        _renderHeatPointList() {
+            if (this._heatLayer) {
+                this.heatmap.removeLayer(this._heatLayer);
             }
-        });
-    };
+            this._heatLayer = L.heatLayer(this.heatPointList,
+                // TODO make this parameter list configurable from the model!
+                // parameters to control the appearance of heat points
+                // see leaflet.heat docu for full spec
+                { radius: 20, blur: 30, max: 1.0 }
+                );
+            this._heatLayer.addTo(this.heatmap);
+        };
 
-    (< any > HeatmapField).prototype._renderViewParameter = function() {
-        this.heatmap.setView([this.viewParameter.center.x, this.viewParameter.center.y], this.viewParameter.zoomFactor);
-    };
-
-    (< any > HeatmapField).prototype._renderHeatPointList = function() {
-        if (this._heatLayer) {
-            this.heatmap.removeLayer(this._heatLayer);
-        }
-        this._heatLayer = L.heatLayer(this.heatPointList,
-            // TODO make this parameter list configurable from the model!
-            // parameters to control the appearance of heat points
-            // see leaflet.heat docu for full spec
-            { radius: 20, blur: 30, max: 1.0 }
-            );
-        this._heatLayer.addTo(this.heatmap);
-    };
-
-    (< any > HeatmapField).prototype._onHeatPointsAdded = function(points) {
-        var i: number;
-        if (this._heatLayer) {
-            for (i = 0; i < points.length; i++) {
-                this._heatLayer.addLatLng(points[i]);
+        _onHeatPointsAdded(points) {
+            var i: number;
+            if (this._heatLayer) {
+                for (i = 0; i < points.length; i++) {
+                    this._heatLayer.addLatLng(points[i]);
+                }
             }
-        }
-    };
+        };
 
-    (< any > HeatmapField).prototype.onModelAction = function(event) {
-        if (event.type === 'heatPointsAdded') {
-            this._onHeatPointsAdded(event.points);
-        } else {
-            (< any > HeatmapField).parent.prototype.onModelAction.call(this, event);
-        }
-    };
+        onModelAction(event) {
+            if (event.type === 'heatPointsAdded') {
+                this._onHeatPointsAdded(event.points);
+            } else {
+                super.onModelAction(event);
+            }
+        };
+
+    }
 }
--- code/widgets/org.eclipse.scout.widgets.heatmap.ui.html/src/main/ts/heatmap/HeatmapFieldLayout.ts
+++ code/widgets/org.eclipse.scout.widgets.heatmap.ui.html/src/main/ts/heatmap/HeatmapFieldLayout.ts
@@ -1,27 +1,29 @@
 
 module scout {
 
-    export var HeatmapFieldLayout = function(field) {
-        this.field = field;
-    };
-    (<any>scout).inherits(HeatmapFieldLayout, (<any>scout).AbstractLayout);
+    export declare class AbstractLayout { };
 
-    /**
-     * @override AbstractLayout.js
-     */
-    HeatmapFieldLayout.prototype.layout = function($container) {
-        var htmlContainer = (< any > scout).HtmlComponent.get($container);
+    export class HeatmapFieldLayout extends scout.AbstractLayout {
 
-        // Because of a bug (?) in Leaflet.js, the canvas size must not get smaller than
-        // 1x1 pixels, otherwise an exception is thrown: "Failed to execute 'getImageData'
-        // on 'CanvasRenderingContext2D': The source width is 0."
-        var size = htmlContainer.getSize();
-        if (size.width === 0 || size.height === 0) {
-            size.width = Math.max(size.width, 1);
-            size.height = Math.max(size.height, 1);
-            (<any>scout).graphics.setSize($container, size);
+        constructor(private field: HeatmapField) {
+            super();
         }
 
-        this.field.heatmap.invalidateSize();
-    };
+        layout($container) {
+            var htmlContainer = (< any > scout).HtmlComponent.get($container);
+
+            // Because of a bug (?) in Leaflet.js, the canvas size must not get smaller than
+            // 1x1 pixels, otherwise an exception is thrown: "Failed to execute 'getImageData'
+            // on 'CanvasRenderingContext2D': The source width is 0."
+            var size = htmlContainer.getSize();
+            if (size.width === 0 || size.height === 0) {
+                size.width = Math.max(size.width, 1);
+                size.height = Math.max(size.height, 1);
+                (<any>scout).graphics.setSize($container, size);
+            }
+
+            this.field.heatmap.invalidateSize();
+        }
+
+    }
 }
